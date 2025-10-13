16af14ea0cb774cc06de13f6e4165bbc5528e359
GuilhermeSantos31 <134153787+GuilhermeSantos31@users.noreply.github.com>
2025-10-13 17:51:05 +0100

    change 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá GUilherme!");
+        System.out.println("Olá Guilherme!");
+        System.out.println("ola");
     }
 }
