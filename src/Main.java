c088ddc6bd964a17ab3d9d6638fda59012fe3dfd
Bernardo <b.lobao@campus.fct.unl.pt>
2025-10-13 17:55:57 +0100

    Adicionar olá

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá GUilherme!");
         System.out.println("ola");
+        System.out.println("Olá 2");
     }
 }
