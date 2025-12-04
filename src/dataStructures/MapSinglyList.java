fdab1d832d02e8356642f8a5863d44ce75882254
Bernardo <b.lobao@campus.fct.unl.pt>
2025-12-04 23:36:37 +0000

    Implementation of MapSinglyList.java

--- src/dataStructures/MapSinglyList.java
+++ src/dataStructures/MapSinglyList.java
@@ -11,11 +11,11 @@ class MapSinglyList<K,V> implements Map<K, V> {
 
 
     private SinglyListNode<Entry<K,V>> head;
-
     private int size;
 
     public MapSinglyList() {
-        //TODO: Left as exercise
+        size = 0;
+        head = null;
     }
 
     /**
@@ -23,10 +23,8 @@ class MapSinglyList<K,V> implements Map<K, V> {
      *
      * @return true if dictionary is empty
      */
-  
     public boolean isEmpty() {
-	//TODO: Left as exercise
-        return false;
+        return size == 0;
     }
 
     /**
@@ -36,8 +34,7 @@ class MapSinglyList<K,V> implements Map<K, V> {
      */
     @Override
     public int size() {
-	//TODO: Left as exercise
-        return 0;
+        return size;
     }
 
     /**
@@ -50,10 +47,15 @@ class MapSinglyList<K,V> implements Map<K, V> {
      */
     @Override
     public V get(K key) {
-        //TODO: Left as exercise
+        SinglyListNode<Entry<K, V>> current = head;
+        while (current != null) {
+            if (current.getElement().key().equals(key))
+                return current.getElement().value();
+            current = current.getNext();
+        }
         return null;
     }
-    
+
 
     /**
      * If there is an entry in the dictionary whose key is the specified key,
@@ -65,9 +67,23 @@ class MapSinglyList<K,V> implements Map<K, V> {
      * @return previous value associated with key,
      * or null if the dictionary does not have an entry with that key
      */
-    
+
     public V put(K key, V value) {
-        //TODO: Left as an exercise.
+        SinglyListNode<Entry<K, V>> current = head;
+
+        // Check if key already exists
+        while (current != null) {
+            if (current.getElement().key().equals(key)) {
+                V oldValue = current.getElement().value();
+                current.setElement(new Entry<>(key, value)); // replace entry
+                return oldValue;
+            }
+            current = current.getNext();
+        }
+
+        // If not found, add new node at head
+        head = new SinglyListNode<>(new Entry<>(key, value), head);
+        size++;
         return null;
     }
 
@@ -81,7 +97,25 @@ class MapSinglyList<K,V> implements Map<K, V> {
      * or null if the dictionary does not an entry with that key
      */
     public V remove(K key) {
-        //TODO: Left as an exercise.
+        SinglyListNode<Entry<K, V>> current = head;
+        SinglyListNode<Entry<K, V>> previous = null;
+
+        while (current != null) {
+            if (current.getElement().key().equals(key)) {
+                V oldValue = current.getElement().value();
+
+                if (previous == null)
+                    head = current.getNext();
+                else
+                    previous.setNext(current.getNext());
+
+                size--;
+                return oldValue;
+            }
+            previous = current;
+            current = current.getNext();
+        }
+
         return null;
     }
 
@@ -99,7 +133,7 @@ class MapSinglyList<K,V> implements Map<K, V> {
      *
      * @return iterator of the values in the dictionary
      */
-@SuppressWarnings({"unchecked","rawtypes"})
+    @SuppressWarnings({"unchecked","rawtypes"})
     public Iterator<V> values() {
         return new ValuesIterator(iterator());
     }
@@ -109,7 +143,7 @@ class MapSinglyList<K,V> implements Map<K, V> {
      *
      * @return iterator of the keys in the dictionary
      */
-@SuppressWarnings({"unchecked","rawtypes"})
+    @SuppressWarnings({"unchecked","rawtypes"})
     public Iterator<K> keys() {
         return new KeysIterator(iterator());
     }
